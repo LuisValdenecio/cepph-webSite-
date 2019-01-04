298822562c1240712d15c9c98898665b4750c440
Luis Valdenêncio <ls04af@gmail.com>
2019-01-04 11:24:39 -0200

    galeria foi modificada

--- js/main.js
+++ js/main.js
@@ -14,88 +14,11 @@ jQuery(function($) {'use strict';
     // the register section
     var registerSection = $('.register');
 
-    //# Reservas timer
-    const day = document.querySelector('#day'),
-          hour = document.querySelector('#hour'),
-          minute = document.querySelector('#minute'),
-          seconds = document.querySelector('#second');
+    // match the intervals at which the image loader changes with the textual content
+    var allCarousellsItems = $('.carousel-inner .item');
 
-    var secCounter = 60;
+    
 
-    var firstCounter = 0, secondCounter = 0, diffCounter = 1;
-
-    function setTimer() {
-        firstCounter++;
-        secondCounter++;
-
-        setSec();
-
-        function setSec() {
-          if (secondCounter % 60 == 0) {
-
-            if ((firstCounter - (secondCounter - diffCounter)) < 10) {
-              seconds.innerText = `0${firstCounter - (secondCounter - diffCounter)}`;
-            } else {
-              seconds.innerText = `${firstCounter - (secondCounter - diffCounter)}`;
-            }
-
-            diffCounter++;
-
-            if (firstCounter % 3600 == 0) diffCounter = 0;  // turning point
-
-          }
-        }
-
-        function setMin() {
-          if (secondCounter % 60 == 0) {
-
-            if ((firstCounter - (secondCounter - diffCounter)) < 10) {
-              seconds.innerText = `0${firstCounter - (secondCounter - diffCounter)}`;
-            } else {
-              seconds.innerText = `${firstCounter - (secondCounter - diffCounter)}`;
-            }
-
-            diffCounter++;
-
-            if (firstCounter % 3600 == 0) diffCounter = 0;  // turning point
-
-          }
-        }
-
-        function setHor() {
-          if (secondCounter % 60 == 0) {
-
-            if ((firstCounter - (secondCounter - diffCounter)) < 10) {
-              seconds.innerText = `0${firstCounter - (secondCounter - diffCounter)}`;
-            } else {
-              seconds.innerText = `${firstCounter - (secondCounter - diffCounter)}`;
-            }
-
-            diffCounter++;
-
-            if (firstCounter % 3600 == 0) diffCounter = 0;  // turning point
-
-          }
-        }
-
-        function setDay() {
-          if (secondCounter % 60 == 0) {
-
-            if ((firstCounter - (secondCounter - diffCounter)) < 10) {
-              seconds.innerText = `0${firstCounter - (secondCounter - diffCounter)}`;
-            } else {
-              seconds.innerText = `${firstCounter - (secondCounter - diffCounter)}`;
-            }
-
-            diffCounter++;
-
-            if (firstCounter % 3600 == 0) diffCounter = 0;  // turning point
-
-          }
-        }
-
-        requestAnimationFrame(setTimer);
-    }
-
-    requestAnimationFrame(setTimer);
+        
+    
 });
